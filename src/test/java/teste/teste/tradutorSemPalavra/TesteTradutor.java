2ccdc5ccf1dbba6cfd3f97f17a1af3e353d0d44b
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2024-01-19 11:34:57 -0300

    ajuste exercicios teste Class pilha

--- src/test/java/teste/teste/tradutorSemPalavra/TesteTradutor.java
+++ src/test/java/teste/teste/tradutorSemPalavra/TesteTradutor.java
@@ -10,13 +10,11 @@ public class TesteTradutor {
         Tradutor t = new Tradutor();
         assertTrue(t.estaVazio());
     }
-
     @Test
     public void umaTraducao() {
         Tradutor t = new Tradutor();
-        t.adicionaPalavra("bom", "good");
+        t.adicionaTraducao("bom", "good");
         assertFalse(t.estaVazio());
         assertEquals("good", t.traduzir("bom"));
-
     }
 }
--- src/test/java/teste/teste/tradutorSemPalavra/Tradutor.java
+++ src/test/java/teste/teste/tradutorSemPalavra/Tradutor.java
@@ -1,17 +1,14 @@
 package teste.teste.tradutorSemPalavra;
 
 public class Tradutor {
-
     private String traducao;
 
     public boolean estaVazio() {
         return (traducao == null);
     }
-
-    public void adicionaPalavra(String palavra, String traducao) {
+    public void adicionaTraducao(String palavra, String traducao) {
         this.traducao = traducao;
     }
-
     public Object traduzir(String palavra) {
         return traducao;
     }
