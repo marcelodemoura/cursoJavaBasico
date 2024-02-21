c6a41e2571eb653d0d3bb04a9c420aa9fbb91914
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2024-02-21 10:43:10 -0300

    ajustes

---
+++ projeto curso NA/projeto-curso-NA/src/main/java/com/projeto/cursoNA/projeto/curso/NA/ProjetoCursoNaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.cursoNA.projeto.curso.NA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoNaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoNaApplication.class, args);
+	}
+
+}
