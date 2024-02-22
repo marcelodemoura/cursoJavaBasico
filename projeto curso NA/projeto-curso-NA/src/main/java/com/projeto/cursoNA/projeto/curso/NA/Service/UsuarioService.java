cf992b4cab59832b993cf885ca0098d7073d75a0
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2024-02-22 10:02:52 -0300

    criado crud para Usuario

--- projeto curso NA/projeto-curso-NA/src/main/java/com/projeto/cursoNA/projeto/curso/NA/Service/UsuarioService.java
+++ projeto curso NA/projeto-curso-NA/src/main/java/com/projeto/cursoNA/projeto/curso/NA/Service/UsuarioService.java
@@ -1,14 +1,29 @@
 package com.projeto.cursoNA.projeto.curso.NA.Service;
 
 import com.projeto.cursoNA.projeto.curso.NA.Entity.Usuario;
-import com.projeto.cursoNA.projeto.curso.NA.Repository.UsuarioRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.awt.print.Pageable;
 
 @Service
 public class UsuarioService {
     private UsuarioService usuarioRepository;
 
+    public Usuario buscarId(Long id) {
+        return usuarioRepository.buscarId(id);
+    }
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.salvar(usuario);
+    }
+    public Usuario atualiza(Usuario usuario) {
+        return usuarioRepository.atualiza(usuario);
+    }
+    public ResponseEntity<Usuario> page(Pageable pageable) {
+        return usuarioRepository.page(pageable);
+    }
+    public ResponseEntity<Usuario>delete(Long id){
+        return usuarioRepository.delete(id);
     }
 }
