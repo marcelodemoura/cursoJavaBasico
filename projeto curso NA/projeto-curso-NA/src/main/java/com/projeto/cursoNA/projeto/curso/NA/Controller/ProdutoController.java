f475af93a43d1e5b06057a386f4f2cdd09ef92d6
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2024-02-28 22:26:15 -0300

    created crud product

--- projeto curso NA/projeto-curso-NA/src/main/java/com/projeto/cursoNA/projeto/curso/NA/Controller/ProdutoController.java
+++ projeto curso NA/projeto-curso-NA/src/main/java/com/projeto/cursoNA/projeto/curso/NA/Controller/ProdutoController.java
@@ -1,12 +1,51 @@
 package com.projeto.cursoNA.projeto.curso.NA.Controller;
 
+import com.projeto.cursoNA.projeto.curso.NA.Entity.Produto;
 import com.projeto.cursoNA.projeto.curso.NA.Service.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produto")
 public class ProdutoController {
 
-private final ProdutoService produtoService;
+    private final ProdutoService produtoService;
 
     public ProdutoController(ProdutoService produtoService) {
         this.produtoService = produtoService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Produto> salvar(@RequestBody @Valid Produto produto) {
+        return ResponseEntity.ok().body(produtoService.salvar(produto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Produto>> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(produtoService.findById(id));
+    }
+
+    @PutMapping("/{Id}")
+    public ResponseEntity<Produto> update(@RequestBody Produto produto) {
+        return ResponseEntity.ok(produtoService.update(produto));
+    }
+
+    @GetMapping
+    public Page<Produto> page(@PageableDefault(size = 20, page = 0) Pageable pageable) {
+        return produtoService.page(pageable);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        produtoService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Deletado com sucesso ");
     }
 }
