8e29c4bcb0ae821b727f71c2852dcf04270d0146
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2024-02-29 21:22:46 -0300

    creado crud para entidade pagamento

---
+++ projeto curso NA/projeto-curso-NA/src/main/java/com/projeto/cursoNA/projeto/curso/NA/Service/PagamentoService.java
@@ -0,0 +1,26 @@
+package com.projeto.cursoNA.projeto.curso.NA.Service;
+
+import com.projeto.cursoNA.projeto.curso.NA.Entity.Pagamento;
+import com.projeto.cursoNA.projeto.curso.NA.Repository.PagamentoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class PagamentoService {
+
+    private final PagamentoRepository pagamentoRepository;
+
+    public PagamentoService(PagamentoRepository pagamentoRepository) {
+        this.pagamentoRepository = pagamentoRepository;
+    }
+
+    public Pagamento save(Pagamento pagamento) {
+        return pagamentoRepository.save(pagamento);
+    }
+    public Object buscarId(Long id) {return pagamentoRepository.findById(id);}
+    public Object buscarTodos(Pagamento pagamento) {
+        return pagamentoRepository.findAll();
+    }
+    public Page<Pagamento> page(Pageable pageable) {return pagamentoRepository.findAll(pageable);}
+    public Pagamento update(Pagamento pagamento) {return pagamentoRepository.save(pagamento);}
+    public void delete(Long id) {pagamentoRepository.deleteById(id);}
+}
