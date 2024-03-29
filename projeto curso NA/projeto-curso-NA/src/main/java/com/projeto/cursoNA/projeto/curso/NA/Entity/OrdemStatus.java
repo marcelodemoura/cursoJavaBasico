4168adfeb7320460ac67ce5c00bcc809a113c315
Marcelo <marcelodemoura14@gmail.com>
2024-03-28 22:49:05 -0300

    alterado class para enum

--- projeto curso NA/projeto-curso-NA/src/main/java/com/projeto/cursoNA/projeto/curso/NA/Entity/OrdemStatus.java
+++ projeto curso NA/projeto-curso-NA/src/main/java/com/projeto/cursoNA/projeto/curso/NA/Entity/OrdemStatus.java
@@ -1,16 +1,4 @@
 package com.projeto.cursoNA.projeto.curso.NA.Entity;
-
-import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-<<<<<<< HEAD
-
-=======
-@NoArgsConstructor
-@Entity
->>>>>>> origin/develop
 public enum OrdemStatus {
 
     esperandoPagamento,
